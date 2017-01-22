4b43a3ffb29611520da4471656d9f9d455ce04c1
Gabiroto <gabriel.sl@outlook.com>
2017-01-22 02:35:21 -0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.set('view-engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log(error);;
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'))
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Express hbs study',
+        welcomeMessage: 'Bem vindo, seu lindo.',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'There was an error in this request',
+    });
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port 3000');
+});
